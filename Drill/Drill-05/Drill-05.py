21cc84b83aca51dda11373bc7c24f2280de357c5
janghopark(notebook) <qkrwkdgh0204@naver.com>
2018-09-17 15:06:15 +0900

    네번째 커밋
    2013182021 박장호

--- Drill/Drill-05/Drill-05.py
+++ Drill/Drill-05/Drill-05.py
@@ -66,12 +66,30 @@ def check3():
         frame = (frame + 1) % 8
         y += 5
         delay(0.03)
-
+def check4():
+    x, y = 535, 470
+    frame = 0
+    while x > 477:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.03)
+    while y > 203:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        y -= 5
+        delay(0.03)
 
 while True:
     #check1()
     #check2()
-    check3()
-
+    #check3()
+    check4()
 close_canvas()
 
