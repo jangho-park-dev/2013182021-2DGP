8506015392db871712342de7a40a35a3d7181a67
janghopark(notebook) <qkrwkdgh0204@naver.com>
2018-11-20 16:24:19 +0900

    Drill-14 1차커밋

---
+++ Drill/Drill-14/ball.py
@@ -0,0 +1,25 @@
+import random
+from pico2d import *
+import game_world
+import game_framework
+
+class Ball:
+    image = None
+
+    def __init__(self):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x = random.randint(100, 700-1)
+        self.y = random.randint(0, 800-1)
+        self.fall_speed = 0
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        self.y -= self.fall_speed * game_framework.frame_time
+
